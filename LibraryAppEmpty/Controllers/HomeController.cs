fd919f4b0aeec941397bc6ed331dced69396bfef
mxt299 <mohan.thirunavukkarasu@sherwin.com>
2014-09-17 11:19:09 -0400

    first commit

---
+++ LibraryAppEmpty/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAppEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Save(TitleSubmitModel model)
+        {
+            return null;
+        }
+
+    }
+
+    public class TitleSubmitModel
+    {
+        public string Title { get; set; }
+    }
+}
