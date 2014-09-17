f79e14a1e64389de63c2ae0875a816df7b6ecd5e
mxt299 <mohan.thirunavukkarasu@sherwin.com>
2014-09-17 14:21:00 -0400

    added fluent migrator and migrations

---
+++ LibraryAppEmpty/Migrations/M01_AddBookTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace LibraryAppEmpty.Migrations
+{
+    [Migration(1)]
+    public class M01_AddBookTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("BOOK").WithColumn("TITLE").AsAnsiString(1000);
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
